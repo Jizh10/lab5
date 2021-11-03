7773a0f700af96e1b5ea3264f82f31566d7908f6
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-11-03 04:37:01 +0000

    debug

--- stepper.py
+++ stepper.py
@@ -34,7 +34,7 @@ class Stepper:
     
     for pin in range(len(self.pins)):
       GPIO.output(self.pins[pin], Stepper.sequence[self.state][pin])
-    self.__delay_us(1000000)
+    self.__delay_us(100000)
   
     self.angle += dir*Stepper.halfStepAngle
    
